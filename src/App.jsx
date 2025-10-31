5b838da463fe57e6c54bffe04439b0ddcfeb8a26
GubbinLubbin <silwalankit27@gmail.com>
2025-10-31 23:17:06 +0530

    Lifted the cart up

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { CheckOut } from "./pages/checkout/checkout";
 import {Orders} from './pages/orders/orders';
 import { Tracking } from "./pages/tracking/Tracking";
 import {Routes,Route} from 'react-router-dom'
+import { useEffect,useState } from "react";
+import axios from "axios";
 export default function App(){
+    const [cart,setCart]=useState([]);
+    useEffect(()=>{
+      axios.get('/api/cart-items')
+      .then((response)=>{
+        setCart(response.data)
+      })
+    },[])
   return (
-    <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/checkout" element={<CheckOut />} />
+        <Route path="/" element={<HomePage cart={cart}/>} />
+        <Route path="/checkout" element={<CheckOut cart={cart}/>} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/tracking" element={<Tracking />} />
       </Routes>
-    </>
   )
 }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,17 +4,13 @@ import "./homepage.css";
 import { Header } from "./header";
 import "./general.css";
 import { convertDollars } from "../../../public/moneyformat";
-export function HomePage() {
+export function HomePage({cart}){
   const [products,setProducts]=useState([]);
-  const [cart,setCart]=useState([]);
+  
   useEffect(()=>{
       axios.get('/api/products')
       .then((response)=>{
         setProducts(response.data)
-      })
-      axios.get('/api/cart-items')
-      .then((response)=>{
-        setCart(response.data)
       })
   },[])
   
