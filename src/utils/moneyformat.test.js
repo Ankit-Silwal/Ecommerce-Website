e07ebdc2d1888ba4e574771c1735bd948b8125c9
GubbinLubbin <silwalankit27@gmail.com>
2025-11-03 06:46:48 +0530

    Create unit test for convertDOllas

---
+++ src/utils/moneyformat.test.js
@@ -0,0 +1,12 @@
+import {it,expect,describe} from 'vitest'
+import { convertDollars } from './moneyformat'
+describe('convertDollars',()=>{
+  it('formats 1999 cents as $19.99',()=>{
+    expect(convertDollars(1999)).toBe('$19.99')
+  });
+
+it('displays 2 decimals',()=>{
+  expect(convertDollars(1090)).toBe('$10.90')
+  expect(convertDollars(100)).toBe('$1.00')
+  })
+})
